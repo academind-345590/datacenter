184a4d649d290a2334eb9802f5bbf0d434e067ef
kv-2 <str_token@mail.ru>
2019-07-12 15:03:46 +0300

    add decorator, properties and methods

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+// import the Input decorator
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-metric',
-  templateUrl: './metric.component.html',
-  styleUrls: ['./metric.component.css']
+  selector: "app-metric",
+  templateUrl: "./metric.component.html",
+  styleUrls: ["./metric.component.css"]
 })
-export class MetricComponent implements OnInit {
+export class MetricComponent {
+  // Declares properties with  Input decoeater and default values
+  @Input() title: string = "";
+  @Input() description: string = "";
+  @Input("used") value: number = 0;
+  @Input("available") max: number = 100;
 
-  constructor() { }
-
-  ngOnInit() {
+  // Method to check if utilixation ezceeds 70%
+  isDanger() {
+    return this.value / this.max > 0.7;
   }
-
 }
