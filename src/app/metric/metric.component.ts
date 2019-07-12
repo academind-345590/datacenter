d51c029921ea7f879f55231bff57257de78ad894
kv-2 <str_token@mail.ru>
2019-07-12 18:35:35 +0300

    add validation to the Metric

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -10,8 +10,31 @@ export class MetricComponent {
   // Declares properties with  Input decoeater and default values
   @Input() title: string = "";
   @Input() description: string = "";
-  @Input("used") value: number = 0;
-  @Input("available") max: number = 100;
+  // private properties for storing the validated values
+  private _value: number = 0;
+  private _max: number = 100;
+
+  //setter methos for value with input decorator
+  @Input("used")
+  set value(value: number) {
+    if (isNaN(value)) value = 0;
+    this._value = value;
+  }
+  //getter merhod for value to return privare property
+  get value(): number {
+    return this._value;
+  }
+
+  //setter methos for max with input decorator
+  @Input("available")
+  set max(max: number) {
+    if (isNaN(max)) max = 100;
+    this._max = max;
+  }
+  //getter merhod for max to return privare property
+  get max(): number {
+    return this._max;
+  }
 
   // Method to check if utilixation ezceeds 70%
   isDanger() {
