a72e9c11df93898a9e43b5a42647f2a6acf6c410
kv-2 <str_token@mail.ru>
2019-07-14 16:07:09 +0300

    revemp erratums

--- src/app/nodes-row/nodes-row.component.ts
+++ src/app/nodes-row/nodes-row.component.ts
@@ -1,19 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
   // use [] to the attribute app-nodes-row
-  selector: '[app-nodes-row]',
-  templateUrl: './nodes-row.component.html',
-  styleUrls: ['./nodes-row.component.css']
+  selector: "[app-nodes-row]",
+  templateUrl: "./nodes-row.component.html",
+  styleUrls: ["./nodes-row.component.css"]
 })
 export class NodesRowComponent {
-
   // Declares the node property as an input
   @Input() node: any;
 
   // Implement the method to check if the value exceeds 70% utilization
-  isDanger(prop){
-    return this.node[prop].used / this.node[prop].availavle > 0.7;
+  isDanger(prop) {
+    return this.node[prop].used / this.node[prop].available > 0.7;
   }
-
 }
