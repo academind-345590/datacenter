529349fcc6cfcd33dfbe9a270e57bf6126ad8f67
kv-2 <str_token@mail.ru>
2019-07-14 14:07:08 +0300

    ng generate component nodes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MetricComponent } from './metric/metric.component';
+import { NodesComponent } from './nodes/nodes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashboardComponent,
-    MetricComponent
+    MetricComponent,
+    NodesComponent
   ],
   imports: [
     BrowserModule,
