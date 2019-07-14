e6dc75db8e17364fd58bcfd8bc1c09a0668c221a
kv-2 <str_token@mail.ru>
2019-07-14 15:32:56 +0300

    update controller row

--- src/app/nodes-row/nodes-row.component.ts
+++ src/app/nodes-row/nodes-row.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-nodes-row',
+  // use [] to the attribute app-nodes-row
+  selector: '[app-nodes-row]',
   templateUrl: './nodes-row.component.html',
   styleUrls: ['./nodes-row.component.css']
 })
-export class NodesRowComponent implements OnInit {
+export class NodesRowComponent {
 
-  constructor() { }
+  // Declares the node property as an input
+  @Input() node: any;
 
-  ngOnInit() {
+  // Implement the method to check if the value exceeds 70% utilization
+  isDanger(prop){
+    return this.node[prop].used / this.node[prop].availavle > 0.7;
   }
 
 }
